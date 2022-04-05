67c35743066fc9c74a82a17c9662c4df701c79a0
lvhaifeng <lvhaifeng@bytedance.com>
2022-04-05 19:18:20 +0800

    feat: 完善serviceHandler方法.

--- handle/service_handler.go
+++ handle/service_handler.go
@@ -1,6 +1,8 @@
 package handle
 
 import (
+	"encoding/json"
+
 	"go.themis.run/themis-web/core"
 	"go.themis.run/themis-web/model"
 	"go.themis.run/themisclient"
@@ -47,4 +49,31 @@ func (s *ServiceHandler) FindAllService() ([]model.Service, error) {
 	}
 
 	return services, nil
+}
+
+func (s *ServiceHandler) FindInstance(serviceName string) ([]model.Instance, error) {
+	instances, err := s.client.DiscoveryService(serviceName)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]model.Instance, 0)
+	for _, v := range instances {
+		m := make(map[string]string)
+		err := json.Unmarshal(v.MetaData, &m)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, model.Instance{
+			ServiceName: v.ServiceName,
+			ClusterName: v.ClusterName,
+			IP:          v.IP,
+			Port:        v.Port,
+			IsHealthy:   v.IsHealthy,
+			Meta:        m,
+		})
+	}
+
+	return res, nil
 }
