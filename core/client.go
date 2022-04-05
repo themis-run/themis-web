0fff491b837a1fe39e8df6f922b56f6d635cae74
lvhaifeng <lvhaifeng@bytedance.com>
2022-04-05 18:07:05 +0800

    feat: 加入themis SDK.

--- core/client.go
+++ core/client.go
@@ -1,7 +1,27 @@
 package core
 
-import "go.themis.run/themisclient"
+import (
+	"sync"
 
-func GetThemisClient() *themisclient.Client {
-	return nil
+	"go.themis.run/themis-web/conf"
+	"go.themis.run/themisclient"
+)
+
+var client *themisclient.Client
+var one sync.Once
+
+func GetThemisClient() (*themisclient.Client, error) {
+	var err error
+	one.Do(func() {
+		serverName, address := conf.GetThemisServer()
+		opt := []themisclient.Option{
+			themisclient.WithServerName(serverName),
+			themisclient.WithServerAddress(address),
+		}
+
+		config := themisclient.NewConfigration(opt...)
+
+		client, err = themisclient.NewClient(config)
+	})
+	return client, err
 }
